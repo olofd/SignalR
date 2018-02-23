cd57364f3b2266659a250a0eafe428b5e5c4e15f
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-02-22 19:46:19 -0800

    remove extra cruft from index.ts

--- client-ts/FunctionalTests/ts/index.ts
+++ client-ts/FunctionalTests/ts/index.ts
@@ -2,12 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 console.log("SignalR Functional Tests Loaded");
-document.write("<input type=\"hidden\" id=\"sentinel-signalr-functional-tests\" value=\"loaded\"></input>");
 
 import "es6-promise/dist/es6-promise.auto.js";
 import "./ConnectionTests";
 import "./HubConnectionTests";
 import "./WebDriverReporter";
 import "./WebSocketTests";
-
-(window as any).functionalTestsLoaded = true;
